2888966672240f977769affdf8973fb1ed47876d
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-07-07 13:13:58 +0900

    add generateFooter command

---
+++ src/convert.ts
@@ -0,0 +1,61 @@
+export function convert(text: string): string {
+   const collected: string[] = [];
+   let isCodeBlock: boolean = false;
+   let isHyperLink: boolean = false;
+   let hyperLink: string = '';
+   let last: string = '';
+   let out: string = '';
+
+   for (let p = 0; p < text.length; p++) {
+      const c = text.charAt(p);
+
+      if (c === '`' && isCodeBlock) {
+         isCodeBlock = false;
+         out += c;
+      } else if (c === '`' && !isCodeBlock) {
+         isCodeBlock = true;
+         out += c;
+      } else if (c === '[' && !isCodeBlock && !isHyperLink) {
+         out += c;
+      } else if (c === '(' && last === ']' && !isCodeBlock) {
+         hyperLink = '';
+         isHyperLink = true;
+      } else if (c === ')' && !isCodeBlock && isHyperLink) {
+         isHyperLink = false;
+         if (hyperLink !== '') {
+            const link = hyperLink;
+            let pointer = -1;
+
+            for (let i = 0; i < collected.length; i++) {
+               if (collected[i] === link) {
+                  pointer = i + 1;
+                  break;
+               }
+            }
+
+            if (pointer === -1) {
+               collected.push(link);
+               pointer = collected.length;
+            }
+
+            out += `[${pointer}]`;
+         } else {
+            out += c;
+         }
+      } else if (!isCodeBlock && isHyperLink) {
+         hyperLink += c;
+      } else {
+         out += c;
+      }
+      last = c;
+   }
+
+   if (collected.length !== 0) {
+      out += '\n\n';
+      for (let i = 0; i < collected.length; i++) {
+         out += `[${i + 1}]:${collected[i]}\n`;
+      }
+   }
+
+   return out;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,20 @@
-import { ExtensionContext } from 'vscode';
+import { window, commands, ExtensionContext, Position, Range } from 'vscode';
+import { convert } from './convert';
 
-export function activate(context: ExtensionContext) { }
+export function activate(context: ExtensionContext) {
+   context.subscriptions.push(
+      commands.registerCommand('vscode-md-footer.generateFooter', generateFooter)
+   );
+}
 
 export function deactivate() { }
+
+function generateFooter() {
+   if (!window.activeTextEditor) return;
+   const editor = window.activeTextEditor;
+   const start = new Position(0, 0);
+   const end = new Position(editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
+   editor.edit(builder => {
+      builder.replace(new Range(start, end), convert(editor.document.getText()));
+   });
+}
