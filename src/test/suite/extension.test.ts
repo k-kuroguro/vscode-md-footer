e0ceac80cdf658f5232c437b2bbd6179d14a9826
k-kuroguro <riku_2002_1128@i.softbank.jp>
2021-07-07 14:56:26 +0900

    add convert test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,21 @@
 import * as assert from 'assert';
-
-//You can import and use all API from the 'vscode' module
-//as well as import your extension to test it
 import * as vscode from 'vscode';
-//import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-   vscode.window.showInformationMessage('Start all tests.');
+   let extension: vscode.Extension<any>;
+   const timeout = async (ms = 200) => new Promise(resolve => setTimeout(resolve, ms));
 
-   test('Sample test', () => {
-      assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-      assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+   suiteSetup(() => {
+      extension = vscode.extensions.getExtension('k-kuroguro.vscode-md-footer') as vscode.Extension<any>;
+   });
+
+   test('Activation test', async () => {
+      await extension.activate();
+      assert.strictEqual(extension.isActive, true);
+   });
+
+   test('Extension loads in VSCode and is active', async () => {
+      await timeout(1500);
+      assert.strictEqual(extension.isActive, true);
    });
 });
